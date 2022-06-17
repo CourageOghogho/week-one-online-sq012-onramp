b1bb80c314312919572593557e94d79c9a565fe9
Courage Oghogho <oghogho.courage@gmail.com>
2022-06-17 23:13:41 +0100

    first assignment

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,8 @@
 function secondLargest(arr) {
+    //author: Courage Oghogho
+    //decagon challenge
+    let arrSorted=arr.sort((a,b)=>{return b-a;});
+    return arrSorted[1];
   
 }
 module.exports = secondLargest
