796609a3ba4df596fde49ab641c0cebdbddbbc8a
Courage Oghogho <oghogho.courage@gmail.com>
2022-06-18 00:19:02 +0100

    third challenge

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,14 @@
 function sumMix(arr) {
+    //author: Courage Oghogho
+    let sum=0;
+    let properArray=arr.map((elem)=>{return parseInt(elem, 10);});
+    properArray.map((elem)=>{return sum+=elem;});
+    return sum;
+
+
    
  }
 
   module.exports = sumMix
+
+  console.log(sumMix([23,1,5,"1","6"]));
