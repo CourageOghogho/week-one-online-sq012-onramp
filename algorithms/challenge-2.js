adc4883fe6dd4ff81822c7cb1fe2604775f59e35
Courage Oghogho <oghogho.courage@gmail.com>
2022-06-20 00:41:39 +0100

    challenge 2 updated

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,6 +1,6 @@
 function filterArray(arr) {
-    return arr.filter((elem)=>{return typeof elem==='number'})
-
+    return arr.filter(function (elem){ return typeof elem === 'number'; });
 }
-
 module.exports = filterArray
+
+console.log(filterArray([2,3,5,"44","Kosy"]))
