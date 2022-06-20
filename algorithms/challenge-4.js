8140a5539152a0ca5b7ff764fcc364ab473e6774
Courage Oghogho <oghogho.courage@gmail.com>
2022-06-20 01:52:58 +0100

    all updated

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,13 +1,14 @@
 
 //author: Courage
 function sumTwoSmallestNums(arr) {
+    //sorted the array in ascending order
+            //n elem
+    // filter negative numbers from the sorted array
+    // loop twice to get get the first two elements
     let sum=0;
-    let filteredArr=arr.filter((elem)=>{ return elem>0; });
-    let sortedFilteredArr=filteredArr.sort((a,b)=>{ return a-b});
-    sum=sortedFilteredArr[0]+sortedFilteredArr[1];
+    let newFilteredArr=arr.filter(function(elem) { return elem>0; });
+    newFilteredArr.sort(function(a,b) { return a-b; });
+    sum=newFilteredArr[0]+newFilteredArr[1];
     return sum;
-    
  }
   module.exports =sumTwoSmallestNums
-
-  console.log(sumTwoSmallestNums([0,-2,-1,-20,2, 3,1,1,23]));
